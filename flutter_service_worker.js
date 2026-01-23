a744a8ae774f01b07f9b9052237de93c1e8c02fd
dildarhussain73 <dildar.hussain@veroke.com>
2026-01-23 15:07:33 +0500

    Rebuild Flutter web with correct base-href

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,7 +44,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5cae79473bec2575e2f9ced4723ce1ca
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "33b0bf80c29390d365fe15fb6863cc60",
+"flutter_bootstrap.js": "7e34bfc20cc42d7ff0cfc445b70dd0ed",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
